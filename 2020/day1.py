3dc3318031efb7c57cee454de05839b915c64404
Matthew Cole <mcole8@binghamton.edu>
2020-12-01 23:58:11 -0500

    Update Day 1 solution

--- 2020/day1.py
+++ 2020/day1.py
@@ -2,21 +2,23 @@
 
 import itertools
 import math
+import typing
+from collections.abc import Iterable
+
+def solve(entries:Iterable,r:int):
+	"""
+	Prints the product of r expense entries whose sum is 2020.
+	"""
+	combs = [(sum(p),math.prod(p)) for p in itertools.combinations(entries,r)]
+	solution = list(filter(lambda c:c[0]==2020, combs))
+	for s,p in solution:
+		print(p)
 
 def main():
 	with open('input1.txt') as fin:
-		# Part 1 
 		entries = [int(line) for line in fin.readlines()]
-		combs = [(sum(p),math.prod(p)) for p in itertools.combinations(entries,2)]
-		solution = list(filter(lambda c:c[0]==2020, combs))
-		for s,p in solution:
-			print(p)
-
-		# Part 2
-		combs = [(sum(p),math.prod(p)) for p in itertools.combinations(entries,3)]
-		solution = list(filter(lambda c:c[0]==2020, combs))
-		for s,p in solution:
-			print(p)
+		solve(entries,2)
+		solve(entries,3)
 
 if __name__ == '__main__':
 	main()
