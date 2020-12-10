891ec0efcd91fef25cd345f635e6554277583614
Matthew Cole <mcole8@binghamton.edu>
2020-12-10 00:18:15 -0500

    Add day10 initial file upload

---
+++ 2020/day10.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+	with open('inputs/input10-test.txt') as fin:
+		adaptors = sorted(int(line) for line in fin.readlines())
+		adaptors.append(max(adaptors) + 3)
+		adaptorsc
+in		
+		
+		
+	
+if __name__ == "__main__":
+	main()
+	
