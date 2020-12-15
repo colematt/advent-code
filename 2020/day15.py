8033040e56be30ce5b6e59706325b5f5978f229e
Matthew Cole <mcole8@binghamton.edu>
2020-12-15 03:25:21 -0500

    Add 2020 Day 15 solution

--- 2020/day15.py
+++ 2020/day15.py
@@ -4,68 +4,77 @@ import collections
 
 def game(opening):
 	"""
-	Generate the terms in a memory game sequence, where at each turn, the term
-	is (step, number).
-	
-	:param      opening:  The opening
-	:type       opening:  { type_description }
+	Generate the terms in a memory game sequence.
+	- Opening is always generated in that order.
+	- Subsequently, for the most recently generated number:
+		- If that was the first time the number has been spoken, 
+		  the current player next says 0.
+		- Otherwise, the number had been spoken before; the current player 
+		  next announces how many turns apart the number is from when it was 
+		  previously spoken.
 	"""
-	# Initialize gamestate
-	turn = 0
-	last = None
-
-	# Build the initial spoken dictionary
+	
+	# Build the initial spoken numbers dictionary
 	spoken = collections.defaultdict(int)
 	spoken.clear()
 	for turn, num in list(enumerate(opening[:-1], start=1)):
 		spoken[num] = turn
-	print(spoken)
 
-	# Generate the opening
+	# Fast forward gamestate, generate the opening
+	last = opening[-1]
+	turn = len(opening) + 1
 	while opening:
-		yield (last := opening.pop(0))
-		turn += 1
-
+		yield (opening.pop(0))
+		
 	while True:
 		# Generate the subsequent turn
-		if last not in spoken: curr = 0
-		else: curr = turn - spoken[last]
-
+		val = spoken[last]
+		if val == 0: speak = 0
+		else: speak = turn - 1 - val
+		yield speak
+		
 		# Update gamestate
 		spoken[last] = turn - 1
-		last = curr
+		last = speak
 		turn += 1
-
-		print(spoken)
-			
-
+		
 def test():
-
 	#### PART 1 ####
 	turns = [0, 3, 6, 0, 3, 3, 1, 0, 4, 0]
 	game1 = game(turns[:3])
 	for t in range(10):
-		print(t+1, curr := next(game1))
-		assert curr == turns[t]
+		assert next(game1) == turns[t]
 
-	# openings = [
-	# 	[1,3,2],
-	# 	[2,1,3],
-	# 	[1,2,3],
-	# 	[2,3,1],
-	# 	[3,2,1],
-	# 	[3,1,2]
-	# ]
-	# answers = [1, 10, 27, 78, 438, 1836]
-	# for opening,answer in zip(openings,answers):
-	# 	game1 = game(opening)
-	# 	for turn in range(2020):
-	# 		last = next(game1)
-	# 	assert last == (answer)
-
-
+	openings = [
+		[1,3,2],
+		[2,1,3],
+		[1,2,3],
+		[2,3,1],
+		[3,2,1],
+		[3,1,2]]
+	answers = [1, 10, 27, 78, 438, 1836]
+	for opening,answer in zip(openings,answers):
+		game1 = game(opening)
+		for turn in range(2020):
+			last = next(game1)
+		assert last == (answer)
+	
+		#### PART 2 ####
+		pass
+	
 def main():
 	opening = [8,11,0,19,1,2]
+	
+	#### PART 1 ####
+	gg = game(opening)
+	for turn in range(1,2020):
+		next(gg)
+	print(next(gg))
+	
+	#### PART 2 ####
+	for turn in range(2021,30000000):
+		next(gg)
+	print(next(gg))
 
 if __name__ == '__main__':
 	test()
