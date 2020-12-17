effbdb1ddc063b4f7f68b461bb6687169d1ba586
Matthew Cole <mcole8@binghamton.edu>
2020-12-17 00:22:08 -0500

    Restructure 2020

---
+++ 2020/17/solution.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import itertools
+
+class pocket:
+	"""
+	Simulation of a pocket dimension, consisting of cubes behaving similar to 
+	Conway's game of life
+	"""
+	
+	def __init__(self, active=None):
+		self.active = active
+		self.t = 0
+	
+	def __repr__(self):
+		pass
+	
+	def __str__(self):
+		pass
+	
+	def step(self):
+		pass
+
+def test():
+	pass
+	
+def main():
+	pass
+
+if __name__ == "__main__":
+	test()
+	main()
+	
