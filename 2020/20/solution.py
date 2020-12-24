f55f544192f693d840e03fb5da90e784f1ca96d3
Matthew Cole <mcole8@binghamton.edu>
2020-12-24 00:07:52 -0500

    Update 2020 Day 20 WIP

--- 2020/20/solution.py
+++ 2020/20/solution.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import aocd
+import copy
 from collections import namedtuple
 from icecream import ic
 import itertools 
@@ -8,28 +9,47 @@ import itertools
 Tile = namedtuple('Tile', ['id','image'])
 
 def rotate(tile):
-    return list(''.join(x[::-1]) for x in zip(*tile))
+	"""
+	Return a copy of a tile whose image has been rotated 90 degrees clockwise
+	"""
+	return Tile(id=tile.id, image=list(''.join(x[::-1]) for x in zip(*tile.image)))
 
 def flip(tile):
-    return list(reversed(tile.copy()))
+    """
+    Return a copy of a Tile whose image has been flipped about the x-axis
+    """
+
+    return Tile(id=tile.id, image=list(reversed(copy.copy(tile.image))))
 
 def build_tile_transformations(tile):
-    tile90 = rotate(tile)
-    tile180 = rotate(tile90)
-    tile270 = rotate(tile180)
-    return [tile, tile90, tile180, tile270, flip(tile), flip(tile90), flip(tile180), flip(tile270)]
+	tile90 = rotate(tile)
+	tile180 = rotate(tile90)
+	tile270 = rotate(tile180)
+	return [tile, tile90, tile180, tile270, flip(tile), flip(tile90), flip(tile180), flip(tile270)]
 
 def top(tile):
-	return tile[0]
+	"""
+	Return the top slice of tile's image
+	"""
+	return tile.image[0]
 
 def bottom(tile):
-	return tile[-1]
+	"""
+	Return the bottom slice of tile's image
+	"""
+	return tile.image[-1]
 
 def left(tile):
-	return [row[0] for row in tile]
+	"""
+	Return the left slice of tile's image
+	"""
+	return "".join([row[0] for row in tile.image])
 
 def right(tile):
-	return [row[-1] for row in tile]
+	"""
+	Return the right slice of tile's image
+	"""
+	return "".join([row[-1] for row in tile.image])
 
 def test():
 	with open('test.txt', 'r') as fin:
@@ -37,15 +57,23 @@ def test():
 	tiles = list()
 	for tile in data.split('\n\n'):
 		_, i, *rows = tile.split()
-		tiles.append(Tile(id=int(i.rstrip(':')), image=[[char for char in row] for row in rows]))
-	ic(tiles)
+		tiles.append(Tile(id=int(i.rstrip(':')), image=rows))
+
+	# Test tile transforms and slices
+	assert tiles[0] == Tile(id=2311, image=['..##.#..#.', '##..#.....', '#...##..#.', '####.#...#', '##.##.###.', '##...#.###', '.#.#.#..##', '..#....#..', '###...#.#.', '..###..###'])
+	assert rotate(tiles[0]) == Tile(id=2311, image=['.#..#####.', '.#.####.#.', '###...#..#', '#..#.##..#', '#....#.##.', '...##.##.#', '.#...#....', '#.#.##....', '##.###.#.#', '#..##.#...'])
+	assert flip(tiles[0]) == Tile(id=2311, image=['..###..###', '###...#.#.', '..#....#..', '.#.#.#..##', '##...#.###', '##.##.###.', '####.#...#', '#...##..#.', '##..#.....', '..##.#..#.'])
+	assert top(tiles[0]) == "..##.#..#."
+	assert bottom(tiles[0]) == "..###..###"
+	assert left(tiles[0]) == ".#####..#."
+	assert right(tiles[0]) == "...#.##..#"
 
 def main():
 	data = aocd.get_data(year=2020, day=20)
 	tiles = list()
 	for tile in data.split('\n\n'):
 		_, i, *rows = tile.split()
-		tiles.append(Tile(id=int(i.rstrip(':')), image=[[char for char in row] for row in rows]))	
+		tiles.append(Tile(id=int(i.rstrip(':')), image=rows))	
 
 if __name__ == '__main__':
 	test()
