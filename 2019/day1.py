6964726124f1e828ed004b65bd00d7cc689746b1
Matthew Cole <mcole8@binghamton.edu>
2021-01-02 00:23:07 -0500

    Submit 2019 Day 1 by aocd

--- 2019/day1.py
+++ 2019/day1.py
@@ -1,18 +1,28 @@
 import sys
 import math
+import aocd
+from icecream import ic
 
 fuel = lambda m: max(math.floor(m/3)-2,0)
 
-if __name__ == "__main__":
+def main():
 	# Read the input
-	with open('inputs/day1.txt', 'r') as f:
-		modules = [int(line) for line in f.readlines()]
+	data = aocd.get_data(year=2019, day=1)
+	modules = [int(line) for line in data.splitlines()]
 	
 	# Fuel for just the modules
-	print("Just modules: ", sum(map(lambda m: fuel(m), modules)))
+	load = sum(map(lambda m: fuel(m), modules))
+	ic('part a:', load)
+	aocd.submit(load, year=2019, day=1, part='a')
+
 	
 	# Fuel for modules + fuel for fuel
 	for	m in modules:
 		if fuel(m) > 0:
-			modules.append(fuel(m))	
-	print("Modules plus fuel: ", sum(map(fuel,modules)))
+			modules.append(fuel(m))
+	load = sum(map(fuel,modules))
+	ic('part b:', load)
+	aocd.submit(load, year=2019, day=1, part='b')
+
+if __name__ == "__main__":
+	main()
