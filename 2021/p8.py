97b1ba6b309e7f687cebdcb850f904b709514d9c
Matthew Cole <mcole8@binghamton.edu>
2021-12-11 22:40:54 -0500

    Add 2021 Day 8 Part B solution

--- 2021/p8.py
+++ 2021/p8.py
@@ -15,41 +15,58 @@ bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbg
 egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
 
-DIGITS = {
-	0 : 'abcefg', 	#6
-	1 : 'cf',		#2 unique
-	2 : 'acdeg',	#5
-	3 : 'acdfg',	#5
-	4 : 'bcdf',		#4 unique
-	5 : 'abdfg',	#5
-	6 : 'abdefg',	#6
-	7 : 'acf',		#3 unique
-	8 : 'abcdefg',	#7 unique
-	9 : 'abcdfg',	#6
-}
-INV_DIGITS = {DIGITS[key]:key for key in DIGITS}
+def entries(lines):
+	return tuple((patterns.rstrip().split(),value.lstrip().split()) 
+		for patterns,value in [line.split('|') for line in lines])
+
+def solveA(entries):
+	filtered = [list(
+		filter(lambda v: len(v) in {2,3,4,7},value)) for _,value in entries]
+	return sum([len(f) for f in filtered])
+
+def solveB(entries):
+	total = 0
+	
+	# split signal and output
+	for signals,outputs in entries:
+		# get number of segments
+		l = {len(s): set(s) for s in signals}    
+
+		# accumulate solved outputs
+		n = ''
+
+		for o in map(set, outputs):              # loop over output digits
+			lo,lol4,lol2 = len(o), len(o&l[4]), len(o&l[2]) # mask with known digits
+			if lo == 2: n += '1'
+			elif lo == 3: n += '7'
+			elif lo == 4: n += '4'
+			elif lo == 7: n += '8'
+			elif lo == 5 and lol4 == 2: n += '2'
+			elif lo == 5 and lol4 == 3 and lol2 == 1: n += '5'
+			elif lo == 5 and lol4 == 3 and lol2 == 2: n += '3'
+			elif lo == 6 and lol4 == 4: n += '9'
+			elif lo == 6 and lol4 == 3 and lol2 == 1: n += '6'
+			elif lo == 6 and lol4 == 3 and lol2 == 2: n += '0'
+			else: raise Error("Reached default case! %s" % o)
+
+		# convert string to int
+		n = int(n)
+		ic(n)
+
+		# accumulate total
+		total += n
+	return total
 
 def test():
 	lines = TEST_DATA.splitlines()
-	entries = tuple((patterns.rstrip().split(),value.lstrip().split()) 
-		for patterns,value in [line.split('|') for line in lines])
-	ic(entries)
-
-	### PART A ###
-	filtered = [list(filter(lambda v: len(v) in {2,3,4,7},value)) for _,value in entries]
-	ic(filtered)
-	assert sum([len(f) for f in filtered]) == 26
+	assert solveA(entries(lines)) == 26
+	assert solveB(entries(lines)) == 61229
 
 def main():
 	ic.disable()
-
 	lines = aocd.data.splitlines()
-	entries = tuple((patterns.rstrip().split(),value.lstrip().split()) 
-		for patterns,value in [line.split('|') for line in lines])
-
-	### PART A ###
-	filtered = [list(filter(lambda v: len(v) in {2,3,4,7},value)) for _,value in entries]
-	aocd.submit(sum([len(f) for f in filtered]), part="a")
+	aocd.submit(solveA(entries(lines)), part="a")
+	aocd.submit(solveB(entries(lines)), part="b")
 
 if __name__ == '__main__':
 	test()
