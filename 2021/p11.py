6c70af81ec55da6bd26121da963d229a2cc00ec7
Matthew Cole <mcole8@binghamton.edu>
2021-12-13 23:13:24 -0500

    Add 2021 Day 11 Part B solution

--- 2021/p11.py
+++ 2021/p11.py
@@ -4,8 +4,9 @@ import aocd
 from collections import deque
 from functools import partial
 from icecream import ic
-from itertools import filterfalse
+from itertools import chain, filterfalse
 from operator import getitem, setitem
+import copy
 
 TESTDATA = """5483143223
 2745854711
@@ -28,72 +29,83 @@ def adjacent(grid,row,col):
 		lambda c: c[0] < 0 or c[1] < 0 or c[0] >= len(grid) or c[1] >= len(grid[c[0]]),
 		[(row+dr, col+dc) for dr in (-1,0,1) for dc in (-1,0,1) if dr != 0 or dc != 0]))
 
-def simulate(grid,start,steps):
-	flash_count = 0 		# Count of flash events
+def step(grid):
+	"""
+	Advance the grid one time step
+	"""
 	flash_set = set() 	# Set of (row,col) that have flashed
 	flash_queue = deque() 	# Ordered deque of (row,col) ready to flash
 
-	for step in range(start,start+steps):
-		# Increase energy level of all by one,
-		# collecting flashers (energy level > 9)
-		for row in range(len(grid)):
-			for col in range(len(grid[row])):
-				val = getter(grid,row,col) + 1
-				setter(grid,row,col,val)
+	# Increase energy level of all by one,
+	# collecting flashers (energy level > 9)
+	for row in range(len(grid)):
+		for col in range(len(grid[row])):
+			val = getter(grid,row,col) + 1
+			setter(grid,row,col,val)
+			if val > 9:
+				flash_queue.append((row,col))
+	ic(flash_queue)
+
+	# Perform flashes while there are still flashers left,
+	# adding new flashers as they reach level > 10
+	while flash_queue:
+		try:
+			flasher = flash_queue.popleft()
+		except IndexError as ie:
+			print("Attempted pop from empty flashers deque!", ie)
+
+		# If this flasher hasn't already flashed, flash it
+		if flasher not in flash_set:
+			flash_set.add(flasher)
+
+			# Increase energy level of adjacents by 1
+			for adj in adjacent(grid,*flasher):
+				val = getter(grid,*adj) + 1
+				setter(grid,*adj,val)
+
+				# Add adjacents to flashers if they level-up
 				if val > 9:
-					flash_queue.append((row,col))
-		ic(flash_queue)
+						flash_queue.append(adj)
 
-		# Perform flashes while there are still flashers left,
-		# adding new flashers as they reach level > 10
-		while flash_queue:
-			try:
-				flasher = flash_queue.popleft()
-			except IndexError as ie:
-				print("Attempted pop from empty flashers deque!", ie)
+	# Flashing has completed, reset flashers to zero
+	for flasher in flash_set:
+		setter(grid,*flasher,0)
 
-			# If this flasher hasn't already flashed, flash it
-			if flasher not in flash_set:
-				flash_set.add(flasher)
+	# Return count of octopii that flashed
+	return len(flash_set)
 
-				# Increase energy level of adjacents by 1
-				for adj in adjacent(grid,*flasher):
-					val = getter(grid,*adj) + 1
-					setter(grid,*adj,val)
-
-					# Add adjacents to flashers if they level-up
-					if val > 9:
- 						flash_queue.append(adj)
-
-		# Flashing has completed, reset flashers to zero
-		for flasher in flash_set:
-			setter(grid,*flasher,0)
-
-		# Increase count by size of the flashed set
-		flash_count += len(flash_set)
-
-		# Status reports!
-		if step <= 10:
-			ic(step,grid,len(flash_set),flash_count)
-		elif step % 10 == 0:
-			ic(step,grid,len(flash_set),flash_count)
-
-		# Reset flashed set and queue
-		flash_set.clear() 
-		flash_queue.clear()
-
+def solveA(grid):
+	flash_count = 0
+	for timestep in range(100):
+		if timestep <= 10 or timestep % 10 == 0:
+			ic(timestep)
+			flash_count += step(grid)
+		else:
+			flash_count += step(grid)
+	
 	# Return the flash count for Part A
 	return flash_count
+
+def solveB(grid):
+	timestep = 0
+	while any(cell!=0 for cell in chain(*grid)):
+		timestep += 1
+		step(grid)
+
+	ic(timestep, grid)
+	return timestep
 
 def test():
 	ic.enable()
 	grid = [[int(n) for n in line] for line in TESTDATA.splitlines()]
-	assert simulate(grid,0,100) == 1656
+	assert solveA(copy.deepcopy(grid)) == 1656
+	assert solveB(copy.deepcopy(grid)) == 195
 
 def main():
 	ic.disable()
 	grid = [[int(n) for n in line] for line in aocd.data.splitlines()]
-	aocd.submit(simulate(grid,0,100), part='a')
+	aocd.submit(solveA(copy.deepcopy(grid)), part='a')
+	aocd.submit(solveB(copy.deepcopy(grid)), part='b')
 
 if __name__ == '__main__':
 	test()
