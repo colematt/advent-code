b73443d832cb97c51fcda146edb1720ef63f918c
Matthew Cole <mcole8@binghamton.edu>
2023-12-28 20:16:44 -0500

    Lint 2023/p3

--- 2023/p1.py
+++ 2023/p1.py
@@ -7,17 +7,17 @@ ic.disable()
 
 digitpattern = re.compile('(?=(\d|one|two|three|four|five|six|seven|eight|nine|zero))')
 digitvalues = {
-	'one':1,
-	'two':2,
-	'three':3,
-	'four':4,
-	'five':5,
-	'six':6,
-	'seven':7,
-	'eight':8,
-	'nine':9,
-	'zero':0,
-	'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'0':0
+    'one':1,
+    'two':2,
+    'three':3,
+    'four':4,
+    'five':5,
+    'six':6,
+    'seven':7,
+    'eight':8,
+    'nine':9,
+    'zero':0,
+    '1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'0':0
 }
 
 testdata = """1abc2
@@ -34,32 +34,34 @@ zoneight234
 7pqrstsixteen"""
 
 def test():
-	lines = [line for line in testdata.split("\n")]
-	digits = [tuple(int(i) for i in filter(str.isdigit, line)) for line in lines]
-	values = [10*t[0] + t[-1] for t in digits]
-	assert sum(values) == 142
+    lines = [line for line in testdata.split("\n")]
+    digits = [tuple(int(i) for i in filter(str.isdigit, line)) for line in lines]
+    values = [10*t[0] + t[-1] for t in digits]
+    assert sum(values) == 142
 
-	lines = [line for line in testdata2.split("\n")]
-	finds = [tuple(digitpattern.findall(line)) for line in lines]
-	ic(finds)
-	digits = [tuple(digitvalues[i] for i in tup) for tup in finds]
-	values = [10*tup[0] + tup[-1] for tup in digits]
-	ic(list(zip(lines,values)))
-	assert sum(values) == 281
+    lines = [line for line in testdata2.split("\n")]
+    finds = [tuple(digitpattern.findall(line)) for line in lines]
+    ic(finds)
+    digits = [tuple(digitvalues[i] for i in tup) for tup in finds]
+    values = [10*tup[0] + tup[-1] for tup in digits]
+    ic(list(zip(lines, values)))
+    assert sum(values) == 281
+
 
 def main():
-	lines = [line for line in aocd.data.split("\n")]
-	digits = [tuple(int(i) for i in filter(str.isdigit, line)) for line in lines]
-	values = [10*t[0] + t[-1] for t in digits]
-	aocd.submit(sum(values),year=2023,day=1,part="a")
+    lines = [line for line in aocd.data.split("\n")]
+    digits = [tuple(int(i) for i in filter(str.isdigit, line)) for line in lines]
+    values = [10*t[0] + t[-1] for t in digits]
+    aocd.submit(sum(values), year=2023, day=1, part="a")
 
-	finds = [tuple(digitpattern.findall(line)) for line in lines]
-	ic(finds)
-	digits = [tuple(digitvalues[i] for i in tup) for tup in finds]
-	values = [10*tup[0] + tup[-1] for tup in digits]
-	ic(list(zip(lines,values)))
-	aocd.submit(sum(values),year=2023,day=1,part="b")
+    finds = [tuple(digitpattern.findall(line)) for line in lines]
+    ic(finds)
+    digits = [tuple(digitvalues[i] for i in tup) for tup in finds]
+    values = [10*tup[0] + tup[-1] for tup in digits]
+    ic(list(zip(lines, values)))
+    aocd.submit(sum(values), year=2023, day=1, part="b")
+
 
 if __name__ == '__main__':
-	test()
-	main()
+    test()
+    main()
