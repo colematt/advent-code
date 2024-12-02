237111494706a64797d6fc1704436aff4c6c4742
Matthew Cole <10990627+colematt@users.noreply.github.com>
2024-12-01 22:39:43 -0500

    Starting 2024

---
+++ 2024/p1.py
@@ -0,0 +1,41 @@
+#!/Users/colemr1/venv/bin/python3
+
+from aocd import data, submit
+from collections import Counter
+from more_itertools import transpose
+
+testdata = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+"""
+
+def solveA(data):
+	# Read from text
+	lists = [tuple(int(x) for x in line.split()) for line in data.splitlines()]
+	# Transpose and sort for problem description
+	left,right = (sorted(l) for l in transpose(lists))
+	# Zip and get sum of differences
+	zipped = list(zip(left,right))
+	return sum((abs(x-y) for x,y in zipped))
+	
+def solveB(data):
+	# Read from text
+	lists = [tuple(int(x) for x in line.split()) for line in data.splitlines()]
+	# Transpose and sort for problem description
+	left,right = (sorted(l) for l in transpose(lists))
+	# Count and get sum of similarity scores
+	counts = Counter(right)
+	return sum(i*counts[i] for i in left)
+	
+if __name__ == "__main__":
+	assert solveA(testdata) == 11
+	submit(solveA(data), part='a')
+	
+	assert solveB(testdata) == 31
+	submit(solveB(data), part='b')
+	
+	
+	
