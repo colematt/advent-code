3a78d368d19d2ede415825fa302fcaabc2061d70
Matthew Cole <10990627+colematt@users.noreply.github.com>
2025-12-03 15:24:07 -0500

    Solve 2015/p8

--- 2015/p8.py
+++ 2015/p8.py
@@ -1,26 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import aocd
-import typing
-import re
+from aocd import data, submit
+from ast import literal_eval
 
-from icecream import ic
-ic.enable()
+def solveA(data:str) -> int | None:
+    lines = tuple(line for line in data.splitlines())
+    rawlens = tuple(len(s) for s in lines)
+    evallens = tuple(len(literal_eval(s)) for s in lines if len(s.strip()) > 0)
+    return sum(rawlens) - sum(evallens)
 
-testlines = (
-	"""""""",
-	"""abc""",
-	"""aaa\"aaa""",
-	"""\x27"""
-	)
 
-def test() -> None:
-	for line in testlines:
-		ic(len(line), len(eval("'" + re.escape(line) + "'")))
+def solveB(data:str) -> int:
+    rawlens = tuple(len(s) for s in data.split("\n"))
+    quotelens = tuple(len('"' + s.replace("\\", "\\\\").replace('"', '\\"') + '"')
+                    for s in data.split("\n") if len(s) > 0)
+    return sum(quotelens) - sum(rawlens)
 
-def main() -> None:
-	pass
 
 if __name__ == "__main__":
-	test()
-	main()
+    submit(str(solveA(data)), part='a')
+    submit(str(solveB(data)), part='b')
