bf017c498afc70e46006e0fef848fa1cca5c50ce
Matthew Cole <10990627+colematt@users.noreply.github.com>
2025-12-22 17:14:57 -0500

    Partial solution for 2025/p10a
    Brute force times out on machine 50. Going to try z3 or gaussian elimination next.

---
+++ 2025/p10.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from aocd import data, submit
+from icecream import ic
+
+testdata = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+"""
+
+def get_goal(lights:str) -> list[bool]:
+    return [True if c == "#" else False for c in lights.strip("[]")]
+
+
+def get_buttons(diagrams:list[str], size:int) -> list[list[bool]]:
+    buttons:list[list[bool]] = list()
+    for diagram in diagrams:
+        diagram = eval(diagram)
+        b = [False for _ in range(size)]
+        if type(diagram) == int:
+            b[diagram] = True
+            buttons.append(b)
+        else:
+            for idx in diagram:
+                b[idx] = True
+            buttons.append(b)
+    return buttons
+
+
+def get_joltages(jolts:str) -> set[int]:
+    return eval(jolts)
+
+
+def solveA(data:str) -> int:
+    # Parse machines
+    lines = [line.split() for line in data.splitlines()]
+    machines = [(get_goal(line[0]), get_buttons(line[1:-1], len(line[0])-2)) for line in lines]
+    counter:int = 0
+
+    # Solve machines
+    for i,machine in enumerate(machines):
+        goal, buttons = machine
+        lights, count = [False for _ in range(len(goal))], 0
+        queue = [(lights, count)]
+        
+        while queue:
+            lights, count = queue.pop(0)
+            # Solution found, stop queue
+            if lights == goal:
+                break
+            # Solution not found, add states from pressing each button to queue
+            else:
+                for button in buttons:
+                    queue.append(([l1 ^ l2 for l1,l2 in zip(lights, button)],count+1))
+        # Solution found, add its count to the counter, 
+        # iterate to next machine
+        counter += count
+        ic(i,counter)
+
+    return counter
+
+
+def solveB(data:str) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    assert solveA(testdata) == 7
+    submit(str(solveA(data)), part='a')
+    # assert solveB(testdata) == 24
+    # submit(str(solveB(data)), part='b')
