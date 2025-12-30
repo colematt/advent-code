6d89fedfab3cc601cb251ac607abdb129f25c980
Matthew Cole <10990627+colematt@users.noreply.github.com>
2025-12-30 15:37:08 -0500

    solve 2025/p11b

--- 2025/p11.py
+++ 2025/p11.py
@@ -2,7 +2,10 @@
 
 from aocd import data, submit
 from icecream import ic
+ic.disable()
 from collections import defaultdict
+import graphlib
+from math import prod
 
 testdataA = """aaa: you hhh
 you: bbb ccc
@@ -14,6 +17,21 @@ fff: out
 ggg: out
 hhh: ccc fff iii
 iii: out
+"""
+
+testdataB = """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
 """
 
 def solveA(data: str) -> int:    
@@ -35,13 +53,66 @@ def solveA(data: str) -> int:
     paths:dict[str,int|None] = {key:None for key in devices}
     paths["you"] = 1
 
-    # Calculate shortest path table
+    # Calculate path table
     def count_paths(device:str) -> int:
         if paths[device] == None:
             paths[device] = sum([count_paths(source) for source in predecessors[device]])
         return paths[device] # pyright: ignore[reportReturnType]
     return(count_paths("out"))
 
+def solveB(data: str) -> int | None:
+    # Parse data successors mapping
+    graph:dict[str,set[str]] = dict()
+    for line in data.splitlines():
+        key = line.split()[0].rstrip(":")
+        values = set(val for val in line.split()[1:])
+        graph[key] = values
+        
+    # Topological sort to determine whether
+    # svr->dac->fft->out or svr->fft->dac->out
+    devices:tuple[str,...] = tuple()
+    so:tuple = ("svr", "dac", "fft", "out")
+    ts = graphlib.TopologicalSorter(graph)
+    try:
+        devices = tuple(ts.static_order())
+        so = tuple(filter(lambda item: item in so, reversed(devices)))
+        ic(so)
+    except graphlib.CycleError:
+        print("Cycle error, cannot sort!")
+
+    # Invert successors mapping in predecessors
+    predecessors = defaultdict(list)
+    for key in graph:
+            for val in graph[key]:
+                predecessors[val].append(key)
+    
+    def count_paths(device:str) -> int:
+        if paths[device] == None:
+            paths[device] = sum([count_paths(source) for source in predecessors[device]])
+        return paths[device] # pyright: ignore[reportReturnType]
+    ans = [0,0,0]
+
+    # Get number of paths from svr->dac/fft
+    paths:dict[str,int|None] = {key:None for key in devices}
+    paths[so[0]] = 1
+    ans[0] = count_paths(so[1])
+
+    # Get number of paths from dac/fft->fft/dac
+    paths:dict[str,int|None] = {key:None for key in devices}
+    paths[so[1]] = 1
+    ans[1] = count_paths(so[2])
+
+    # Get number of paths from fft/dac->out
+    paths:dict[str,int|None] = {key:None for key in devices}
+    paths[so[2]] = 1
+    ans[2] = count_paths(so[3])
+
+    # Get total paths from svr->out
+    return prod(ans)
+    
+
 if __name__ == "__main__":
     assert solveA(testdataA) == 5
     submit(str(solveA(data)), part='a')
+    assert solveB(testdataB) == 2
+    submit(str(solveB(data)), part='b')
